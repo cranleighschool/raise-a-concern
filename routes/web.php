4ad52a6372add64bccff2f0cfd82315fe4968df7
Fred Bradley <code@fredbradley.co.uk>
2022-07-18 14:58:34 +0100

    test: callback

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('callback', function(\Illuminate\Http\Request $request) {
+    \Illuminate\Support\Facades\Log::debug($request->all());
+});
