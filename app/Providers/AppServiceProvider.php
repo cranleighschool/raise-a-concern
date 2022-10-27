605379bea12a2b4d7d481dffc2c9261f8cbe5aaa
Fred Bradley <code@fredbradley.co.uk>
2022-10-27 11:21:20 +0100

    refactor - security - update composer deps and add security to service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Everything strict, all the time.
+        // (https://planetscale.com/blog/laravels-safety-mechanisms)
+        Model::shouldBeStrict();
+
+        // In production, merely log lazy loading violations.
+        if ($this->app->isProduction()) {
+            Model::handleLazyLoadingViolationUsing(function ($model, $relation) {
+                $class = get_class($model);
+
+                info("Attempted to lazy load [{$relation}] on model [{$class}].");
+            });
+        }
     }
 }
