9f104a020b3b0a1d5a446813a28bb2e29e9849c1
Fred Bradley <code@fredbradley.co.uk>
2022-11-17 11:55:01 +0000

    correct NRB name

--- config/people.php
+++ config/people.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'CHAIR_OF_GOVERNORS' => 'Mr Adrian Lajtha',
-    'CPS_HEAD' => 'Mr Neil Brookstein',
+    'CPS_HEAD' => 'Mr Neil Brooks',
     'CS_HEAD' => 'Mr Martin Reader'
 ];
