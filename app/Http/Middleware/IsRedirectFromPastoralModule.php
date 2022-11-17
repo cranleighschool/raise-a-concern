1a5b6ed48ac737e6280112372c6b8e0741ee8b9f
Fred Bradley <code@fredbradley.co.uk>
2022-11-17 16:19:00 +0000

    add redirect and middleware to catch redirect

---
+++ app/Http/Middleware/IsRedirectFromPastoralModule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsRedirectFromPastoralModule
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->has('from') && $request->get('from')==='pm') {
+            session()->flash('alert-warning', 'The Pastoral Module is only accessible from within the Cranleigh network. However if you wish to raise a concern you can now do this following the instructions below.');
+        }
+        return $next($request);
+    }
+}
