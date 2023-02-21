c21647c72b55528b1a3652c51d3d9277752273c5
Fred Bradley <code@fredbradley.co.uk>
2023-02-21 14:14:05 +0000

    refactor: amend thank you wording

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -21,19 +21,24 @@
     <div class="alert alert-success">
         <p class="lead">Thank you for submitting your concern.</p>
         <p>This will now be reviewed by {{ $reviewer }}.</p>
-        <p>Your reference number is #{{ $concernId }}.</p>
+        <p>Your reference number is <strong>#{{ $concernId }}</strong>.</p>
         @if (optional(auth()->user())->sso_type === 'parents')
             <p>There is no online facility to check the status of your concern at present, but you are welcome to speak
-                directly to a member of the safeguarding team using the details on this page, if you do not receive a
+                directly to a member of the safeguarding team using the details on this page, if you do not receive suitable
                 response. (Please quote the reference number)</p>
         @elseif(auth()->guest())
             <p>Because you were not logged in there is no automated way for us to contact you again. If you happened to
-                leave contact details within your concern then {{ $reviewer }} will use that if required. Otherwise, if
-                you want to follow up on your concern please contact us again leaving a name and contact details.</p>
+                leave contact details within your concern then {{ $reviewer }} will use that if required.</p>
+            <p>Otherwise, if
+                you want to follow up on your concern please contact us using the details on this page, leaving a name and contact details and your reference number above.</p>
         @else
             <p>You are welcome to log into the <a href="https://pastoral.cranleigh.org">Pastoral Module</a>, when on the School campus, to check the status of your
                 concern. Alternatively you can speak directly to a member of the safeguarding team using the details on
                 this page.</p>
         @endif
     </div>
+    <h3>Where now?</h3>
+    <a href="{{ route('home') }}" class="btn btn-cranleigh">Home</a>
+    <a href="https://www.cranleigh.org" class="btn btn-cranleigh">cranleigh.org</a>
+    <a href="https://www.cranprep.org" class="btn btn-cranprep">cranprep.org</a>
 @endsection
