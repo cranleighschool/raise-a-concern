a936d93b43d038c306c8daa91e2a79628fc11638
Fred Bradley <code@fredbradley.co.uk>
2023-08-08 21:01:12 +0100

    update health checks

--- app/Providers/HealthCheckServiceProvider.php
+++ app/Providers/HealthCheckServiceProvider.php
@@ -4,19 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\PingCheck;
 use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
 use Spatie\Health\Facades\Health;
 
 class HealthCheckServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
     /**
      * Bootstrap services.
      */
@@ -25,6 +18,9 @@ class HealthCheckServiceProvider extends ServiceProvider
         Health::checks([
             UsedDiskSpaceCheck::new(),
             DatabaseCheck::new(),
+            PingCheck::new()->name('Pastoral Ping')->url('https://pastoral.cranleigh.org'),
+            PingCheck::new()->name('Senior Firefly Ping')->url(url('login/firefly/senior')),
+            PingCheck::new()->name('Prep Firefly Ping')->url(url('login/firefly/prep')),
         ]);
 
     }
