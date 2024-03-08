0fd25959b58e9a4817a0251067015539b51023c4
Fred Bradley <code@fredbradley.co.uk>
2024-03-08 09:02:44 +0000

    update CSP

--- resources/views/layouts/raiseaconcern.blade.php
+++ resources/views/layouts/raiseaconcern.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="default-src https:">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'">
     <meta http-equiv="Referrer-Policy" content="no-referrer, strict-origin-when-cross-origin">
 
 
