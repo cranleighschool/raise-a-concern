e2f3504e31cea5f6ca097e49f283d0d70ab4843d
Fred Bradley <code@fredbradley.co.uk>
2024-03-08 16:15:43 +0000

    remove duplicate

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -22,7 +22,6 @@ class SecurityHeaders
         if (app()->environment('production')) {
             $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
             $response->headers->set('X-XSS-Protection', '1; mode=block');
-            $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
             $response->headers->set('Expect-CT', 'enforce, max-age=30');
             $response->headers->set('Permissions-Policy', 'autoplay=(self), camera=(), encrypted-media=(self), fullscreen=(), geolocation=(self), gyroscope=(self), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=(self), usb=()');
             $response->headers->set('Access-Control-Allow-Origin', '*.cranleigh.org');
