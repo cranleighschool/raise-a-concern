cac4a6a9b2069ee07509f16223238a56f7015444
Fred Bradley <code@fredbradley.co.uk>
2024-03-08 14:18:36 +0000

    add health check to get pm user

---
+++ app/Http/HealthChecks/PastoralModuleApiConnectionCheck.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\HealthChecks;
+
+use Illuminate\Support\Facades\Http;
+use Spatie\Health\Checks\Check;
+use Spatie\Health\Checks\Result;
+
+class PastoralModuleApiConnectionCheck extends Check
+{
+    public function run(): Result
+    {
+        $result = Result::make();
+        try {
+            $user = Http::pastoralModule()->get('auth/me')->throw()->collect()->first();
+        } catch (\Exception $e) {
+            return $result->failed('Error thrown whilst trying to access Pastoral Module API User');
+        }
+        if (is_null($user)) {
+            return $result->failed('Pastoral Module API User is inaccessible');
+        }
+
+        if ($user->username === 'RAISEACONCERNAPP' && $user->enabled === true) {
+            return $result->ok('Pastoral Module API User is accessible');
+        }
+        return $result->warning('Pastoral Module API User is not accessible. Unknown issue.');
+    }
+
+}
--- app/Providers/HealthCheckServiceProvider.php
+++ app/Providers/HealthCheckServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\HealthChecks\PastoralModuleApiConnectionCheck;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Health\Checks\Checks\DatabaseCheck;
 use Spatie\Health\Checks\Checks\PingCheck;
@@ -19,6 +20,7 @@ class HealthCheckServiceProvider extends ServiceProvider
             UsedDiskSpaceCheck::new(),
             DatabaseCheck::new(),
             PingCheck::new()->name('Pastoral Ping')->url('https://pastoral.cranleigh.org'),
+            PastoralModuleApiConnectionCheck::new(),
             //PingCheck::new()->name('Senior Firefly Ping')->url(url('login/firefly/senior')),
             //PingCheck::new()->name('Prep Firefly Ping')->url(url('login/firefly/prep')),
         ]);
