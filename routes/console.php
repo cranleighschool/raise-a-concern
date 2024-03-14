27fc7cf2f81a3086f039f76f98bfe0fe25c20233
Fred Bradley <code@fredbradley.co.uk>
2024-03-14 14:31:25 +0000

    fix: console.php

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::call(\Spatie\Health\Commands\RunHealthChecksCommand::class)->everyMinute();
+Schedule::command(\Spatie\Health\Commands\RunHealthChecksCommand::class)->everyMinute();
