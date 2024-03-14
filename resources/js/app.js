6a529f5a74232f83406a399e833b950a86dab863
Fred Bradley <code@fredbradley.co.uk>
2024-03-14 16:32:16 +0000

    javascript

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,11 @@ import './bootstrap';
 
 import 'bootstrap';
 import '@popperjs/core';
+
+if (document.getElementById('logout-btn')) {
+    document.getElementById('logout-btn')
+        .addEventListener('click', function (e) {
+            e.preventDefault();
+            document.getElementById('logout-form').submit();
+        })
+}
