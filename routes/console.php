45dbe9ac1d0abdb414337446edbc20f9bd3fb8f5
Fred Bradley <code@fredbradley.co.uk>
2024-03-14 14:26:12 +0000

    health check command

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,5 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-use Spatie\Health\Commands\RunHealthChecksCommand;
 
-Schedule::call(RunHealthChecksCommand::class)->everyMinute();
+Schedule::call(\Spatie\Health\Commands\RunHealthChecksCommand::class)->everyMinute();
