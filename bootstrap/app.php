5874c029ea1ac2a10ba91624f963fd2c818c84fe
Fred Bradley <code@fredbradley.co.uk>
2024-03-15 14:42:06 +0000

    dd

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,7 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
+        dd(request()->all());
 //        $middleware->validateCsrfTokens(except: ['logout']);
         $middleware->redirectGuestsTo('/login');
         $middleware->redirectUsersTo('/submit');
