9936251cfa23b0bc10742f6dd4e0eac3e522cfcc
Fred Bradley <code@fredbradley.co.uk>
2024-03-22 14:44:10 +0000

    remove debugs

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,6 @@ Route::group([], function () {
     ]);
 });//->middleware(\Spatie\Csp\AddCspHeaders::class);
 
-Route::view('test', 'test')->name('test');
-Route::post('testpost', function (\Illuminate\Http\Request $request) {
-    $sessionDebug = \Illuminate\Support\Facades\Cache::get('sessionDebugData');
-    $sessionDebug['csrf'] = csrf_token();
-    $sessionDebug['request'] = $request->all();
-    dd($sessionDebug);
-})->name('testpost');
-
 // The below are not part of the CSP
 Route::get('health', \Spatie\Health\Http\Controllers\HealthCheckResultsController::class)->name('health');
 Route::get('health.json', \Spatie\Health\Http\Controllers\HealthCheckJsonResultsController::class)->name('health.json');
