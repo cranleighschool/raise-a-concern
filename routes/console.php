d9c9673a76c55a112a1f17e803cb44fa4bb7d13c
Fred Bradley <code@fredbradley.co.uk>
2024-04-09 15:43:50 +0100

    add a console route to describe that the app is back online

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(\Spatie\Health\Commands\RunHealthChecksCommand::class)->everyMinute();
+
+Artisan::command('announce-alive', function () {
+    \Illuminate\Support\Facades\Log::info('The app has reincarnated! (it\'s ready to use again!)');
+})->purpose('For alerting us on slack, that the app has been deployed.')->describe('Announce that the app is alive.');
