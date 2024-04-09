7ab855163eac1237b2b49fbc543d73e0dd2f4674
Fred Bradley <code@fredbradley.co.uk>
2024-04-09 16:25:24 +0100

    fix: livewire issue

--- app/Livewire/SelfReflection.php
+++ app/Livewire/SelfReflection.php
@@ -2,22 +2,23 @@
 
 namespace App\Livewire;
 
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class SelfReflection extends Component
 {
-    public object $reflection;
-    public object $current;
+    private object $reflection;
+    private object $current;
     public int $reportCycleId;
 
-    public function mount(object $reflection, object $current, int $reportCycleId)
+    public function mount(object $reflection, object $current, int $reportCycleId): void
     {
         $this->reflection = $reflection;
         $this->current = $current;
         $this->reportCycleId = $reportCycleId;
     }
 
-    public function render()
+    public function render(): View
     {
         return view('livewire.self-reflection', [
             'reflection' => $this->reflection,
