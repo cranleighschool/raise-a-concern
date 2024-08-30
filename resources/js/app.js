5b7f071d5881b4d933745043e40db773d897a2ce
Fred Bradley <hello@fredbradley.co.uk>
2024-08-30 16:33:49 +0100

    trying this

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,37 @@ import 'bootstrap';
 import '@popperjs/core';
 import tinymce from 'tinymce';
 
+
+/* Default icons are required. After that, import custom icons if applicable */
+import 'tinymce/icons/default/icons.min.js';
+
+/* Required TinyMCE components */
+import 'tinymce/themes/silver/theme.min.js';
+import 'tinymce/models/dom/model.min.js';
+
+/* Import a skin (can be a custom skin instead of the default) */
+import 'tinymce/skins/ui/oxide/skin.js';
+
+/* Import plugins */
+import 'tinymce/plugins/advlist';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/emoticons';
+import 'tinymce/plugins/emoticons/js/emojis';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/table';
+
+/* Import premium plugins */
+/* NOTE: Download separately and add these to /src/plugins */
+/* import './plugins/<plugincode>'; */
+
+/* content UI CSS is required */
+import contentUiSkinCss from 'tinymce/skins/ui/oxide/content.js';
+
+/* The default content CSS can be changed or replaced with appropriate CSS for the editor content. */
+import contentCss from 'tinymce/skins/content/default/content.js';
+
+/* Initialize TinyMCE */
 tinymce.init({
     selector: 'textarea.wysiwyg',
     menubar: false,
