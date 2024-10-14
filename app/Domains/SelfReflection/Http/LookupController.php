913beb2b7b1f8bdefc47dec9081b94e904609cbb
Fred Bradley <hello@fredbradley.co.uk>
2024-10-14 11:04:10 +0100

    show 403 and error page when staff member tries to use pupil page

--- app/Domains/SelfReflection/Http/LookupController.php
+++ app/Domains/SelfReflection/Http/LookupController.php
@@ -10,6 +10,10 @@ class LookupController
     public function __invoke(int $reportCycle): RedirectResponse
     {
         $pupilData = new PupilData();
+        if (auth()->user()->isStaff()) {
+            abort(403, 'This page is intended for pupils and parents only. Staff should use the self-reflection page withing their Teaching Set in the pastoral module.');
+        }
+
 
         return redirect()->route('selfreflection.showget', ['reportCycle' => $reportCycle, 'pupilId' => $pupilData->pupil_id]);
 
