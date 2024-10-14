a5d65b9a89a92581087ec39c6682487bfcb0e73e
Fred Bradley <hello@fredbradley.co.uk>
2024-10-14 11:05:45 +0100

    whoopes, abort before the query is made

--- app/Domains/SelfReflection/Http/LookupController.php
+++ app/Domains/SelfReflection/Http/LookupController.php
@@ -9,10 +9,11 @@ class LookupController
 {
     public function __invoke(int $reportCycle): RedirectResponse
     {
-        $pupilData = new PupilData();
         if (auth()->user()->isStaff()) {
             abort(403, 'This page is intended for pupils and parents only. Staff should use the self-reflection page withing their Teaching Set in the pastoral module.');
         }
+
+        $pupilData = new PupilData();
 
 
         return redirect()->route('selfreflection.showget', ['reportCycle' => $reportCycle, 'pupilId' => $pupilData->pupil_id]);
