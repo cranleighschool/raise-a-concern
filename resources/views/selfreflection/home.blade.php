293e7a600c2b85afaaba5044a2a2fb2c9080f466
Fred Bradley <hello@fredbradley.co.uk>
2024-10-14 08:29:14 +0100

    change text on pupil welcome

--- resources/views/selfreflection/home.blade.php
+++ resources/views/selfreflection/home.blade.php
@@ -18,18 +18,15 @@
         @if (auth()->user()->isPupil())
 
             <div class="mb-3">
-                <p>Following your summer assessments, we would like you to reflect on your progress in each of the subjects you will carry on next year for your GCSEs. Your teachers will see these comments before writing their end of term reports and they will form an important part of the discussion with your tutor. Your comments will also be sent home to your parents/guardians.
-                </p>
+                <p>As parents' meetings approach, we would like you to reflect on your progress in each of your subjects. Your teachers will see these comments before meeting your paretns after half term and they will form an important part of the discussion with your tutor.</p>
                 <p>
                     The things you should be focussing on in your comments are:</p>
                     <ol>
-                    <li>
-                       Your effort and engagement in class and for prep throughout the year
-                    </li>
-                    <li>What went well and what would you do differently next year when it comes to revising for your assessments</li>
-                    <li>Specific things you feel you need to work on to improve at the start of next year</li>
+                    <li>Your effort and engagement in class and for prep so far this term</li>
+                    <li>What has gone well and what would you do differently after half term</li>
+                    <li>Specific things you feel you need to work on to improve</li>
                 </ol>
-                <p>This is not an opportunity for you to comment on your teacher, or indeed the subjects more generally. This is all about you, your strengths and weaknesses and what you intend to do in order to reach your potential in your GCSEs. You should be aiming to write 4/5 lines for each subject and your teacher may give you additional prompts/suggestions.
+                <p>This is not an opportunity for you to comment on your teacher, or indeed the subjects more generally. This is all about you, your strengths and weaknesses and what you intend to do in order to reach your potential in your GCSEs or A-Levels/BTECs. You should be aiming to write 4/5 lines for each subject and your teacher may give you additional prompts/suggestions.
                 </p>
             </div>
         <div class="mb-3">
