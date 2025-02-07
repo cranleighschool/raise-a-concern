f196cb93cc203fe913f02b6aa7fe760581f263f0
Fred Bradley <hello@fredbradley.co.uk>
2025-02-07 13:27:17 +0000

    add new health check to service provider

--- app/Providers/HealthCheckServiceProvider.php
+++ app/Providers/HealthCheckServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\HealthChecks\IsamsBatchApiHealthCheck;
 use App\Http\HealthChecks\PastoralModuleApiConnectionCheck;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Health\Checks\Checks\DatabaseCheck;
@@ -21,6 +22,7 @@ class HealthCheckServiceProvider extends ServiceProvider
             DatabaseCheck::new(),
             PingCheck::new()->name('Pastoral Ping')->url('https://pastoral.cranleigh.org'),
             PastoralModuleApiConnectionCheck::new()->name('Api User'),
+            IsamsBatchApiHealthCheck::new()->name('Isams Batch Api'),
             //PingCheck::new()->name('Senior Firefly Ping')->url(url('login/firefly/senior')),
             //PingCheck::new()->name('Prep Firefly Ping')->url(url('login/firefly/prep')),
         ]);
