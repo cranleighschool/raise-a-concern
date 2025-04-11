6ad88d084ee37b51d40c1b8c5ce091d40239bcd4
Fred Bradley <hello@fredbradley.co.uk>
2025-04-11 16:31:40 +0100

    add custom nonce generator

---
+++ app/Http/NonceGenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Support\Facades\Vite;
+use Spatie\Csp\Nonce\RandomString;
+
+class NonceGenerator extends RandomString
+{
+    public function generate(): string
+    {
+        return Vite::cspNonce();
+    }
+}
