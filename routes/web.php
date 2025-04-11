c02bb1b63088d9bf966bc581e2b8a2c7ab98fd50
Fred Bradley <hello@fredbradley.co.uk>
2025-04-11 16:11:43 +0100

    add report route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use Spatie\Health\Http\Controllers\HealthCheckJsonResultsController;
 use Spatie\Health\Http\Controllers\HealthCheckResultsController;
@@ -8,3 +7,7 @@ use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 // The below are not part of the CSP
 Route::get('health', HealthCheckResultsController::class)->name('health');
 Route::get('health.json', HealthCheckJsonResultsController::class)->name('health.json');
+
+Route::post('csp-report', function () {
+    return response()->json(['status' => 'ok']);
+})->name('csp-report');
