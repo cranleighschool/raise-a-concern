17b41c3cd6aa978fd036bf3ca0aadc7cbb9a22f4
Fred Bradley <hello@fredbradley.co.uk>
2025-04-11 16:22:25 +0100

    unsafe inline

--- app/Http/ContentSecurityPolicy.php
+++ app/Http/ContentSecurityPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Spatie\Csp\Directive;
+use Spatie\Csp\Keyword;
 use Spatie\Csp\Policy;
 use Spatie\Csp\Presets\Basic;
 
@@ -12,11 +13,11 @@ class ContentSecurityPolicy extends Basic
     {
         parent::configure($policy);
         if (app()->environment('production')) {
-//            $policy
-//                ->add(Directive::SCRIPT, 'self')
-//                ->add(Directive::STYLE, 'self')
-//                ->addNonce(Directive::SCRIPT)
-//                ->addNonce(Directive::STYLE);
+            //            $policy
+            //                ->add(Directive::SCRIPT, 'self')
+            //                ->add(Directive::STYLE, 'self')
+            //                ->addNonce(Directive::SCRIPT)
+            //                ->addNonce(Directive::STYLE);
 
             $policy->add(Directive::STYLE, 'fonts.googleapis.com')
                 ->add(Directive::CONNECT, 'cdn.tiny.cloud')
@@ -24,7 +25,8 @@ class ContentSecurityPolicy extends Basic
                 ->add(Directive::STYLE, 'cdn.tiny.cloud')
                 ->add(Directive::FONT, 'fonts.gstatic.com')
                 ->add(Directive::FONT, 'data:')
-                //->add(Directive::FONT, 'self')
+                // ->add(Directive::FONT, 'self')
+                ->add(Directive::SCRIPT, Keyword::UNSAFE_INLINE)
                 ->add(Directive::IMG, 'sp.tinymce.com')
                 ->add(Directive::IMG, 'www.cranleigh.org')
                 ->add(Directive::STYLE, 'rsms.me'); // for laravel health check page
