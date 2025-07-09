9d1dc2a466debb206b3091357c1e0958ed9923a8
Fred Bradley <hello@fredbradley.co.uk>
2025-07-09 08:46:17 +0100

    a1lly - fix login links to remove duplicates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,21 +3,22 @@
 @section('content')
 
     <h2>Welcome</h2>
-    <p class="lead pb-4 mb-0">Please click the relevant button below. You will be directed to Firefly which will manage the login
+    <p class="lead pb-4 mb-0">Please click the relevant button below. You will be directed to Firefly which will manage
+        the login
         process.</p>
 
     <div class="row">
         <div class="col-6">
             <div class="card login-card">
                 <a href="{{ route('raiseaconcern.firefly-login', 'senior') }}">
-                    <x-logo class="card-img-top login-card"/>
+                    <x-logo class="card-img-top p-5"/>
                 </a>
                 <div class="card-body">
                     <h3 class="card-title text-blue">Senior School</h3>
                     <p class="card-text">Parents, Pupils and Staff login with Firefly below.</p>
                     <div class="d-grid">
                         <a href="{{ route('raiseaconcern.firefly-login', 'senior') }}"
-                           class="btn btn-gold-fill">Login with
+                           class="btn btn-gold-fill stretched-link">Login with
                             <span class="visually-hidden">Senior School</span> Firefly</a>
                     </div>
                 </div>
@@ -26,14 +27,14 @@
 
         <div class="col-6">
             <div class="card login-card">
-                <a href="{{ route('raiseaconcern.firefly-login', 'prep')}}">
-                    <x-logo school="CPS" class="card-img-top login-card"/>
-                </a>
+                <x-logo school="CPS" class="card-img-top p-5"/>
                 <div class="card-body">
                     <h3 class="card-title text-blue">Prep School</h3>
                     <p class="card-text">Parents, Pupils and Staff login with Firefly below.</p>
                     <div class="d-grid">
-                        <a href="{{ route('raiseaconcern.firefly-login', 'prep') }}" class="btn btn-gold-fill">Login with <span class="visually-hidden">Prep School</span>
+                        <a href="{{ route('raiseaconcern.firefly-login', 'prep') }}"
+                           class="btn btn-gold-fill stretched-link">Login with <span
+                                class="visually-hidden">Prep School</span>
                             Firefly</a>
                     </div>
                 </div>
@@ -41,8 +42,10 @@
         </div>
     </div>
 
-    <p class="lead text-center pt-4">In most cases it's best if we know who is raising the concern, in case there are further
+    <p class="lead text-center pt-4">In most cases it's best if we know who is raising the concern, in case there are
+        further
         details we
-        need to in order keep someone safe.<br />However, if you really want to remain anonymous <a href="{{ route('raiseaconcern.submit') }}">you can do that too.</a></p>
+        need to in order keep someone safe.<br/>However, if you really want to remain anonymous <a
+            href="{{ route('raiseaconcern.submit') }}">you can do that too.</a></p>
 
 @endsection
